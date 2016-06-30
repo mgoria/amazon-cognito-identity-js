b46542cd1b9a43cae4eaaf2b34d4a77e0c408ffa
mgoria <mgoria@mitocgroup.com>
2016-06-30 18:36:58 +0300

    Make this lib loadable by webpack

---
+++ index.js
@@ -0,0 +1,9 @@
+require('./src/AuthenticationDetails');
+require('./src/AuthenticationHelper');
+require('./src/CognitoAccessToken');
+require('./src/CognitoIdToken');
+require('./src/CognitoRefreshToken');
+require('./src/CognitoUser');
+require('./src/CognitoUserAttribute');
+require('./src/CognitoUserPool');
+require('./src/CognitoUserSession');
